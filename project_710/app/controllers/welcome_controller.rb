20656f4da9592ee8980dd0818b4561401a83c349
EC2 Default User <ec2-user@ip-172-31-30-238.ec2.internal>
2021-12-13 12:30:29 +0000

    ET 718 710 project

---
+++ project_710/app/controllers/welcome_controller.rb
@@ -0,0 +1,26 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  def p2
+  end
+  def p3
+  end
+  def p4
+  end
+  def p5
+  end
+  def p6
+  end
+  def p7
+  end
+  def p8
+  end
+  def p9
+  end
+  def p10
+  end
+  def p11
+  end
+  def p12
+  end
+end
---
+++ project_710/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/index'
+  get 'welcome/p2'
+  get 'welcome/p3'
+  get 'welcome/p4'
+  get 'welcome/p5'
+  get 'welcome/p6'
+  get 'welcome/p7'
+  get 'welcome/p8'
+  get 'welcome/p9'
+  get 'welcome/p10'
+  get 'welcome/p11'
+  get 'welcome/p12'
+  root 'welcome#index'
+end
